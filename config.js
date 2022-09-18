d6ca73ba941e84f2f1470f7636c7a87ab2b139f8
dntt315 <113818501+dntt315@users.noreply.github.com>
2022-09-18 14:52:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,8 @@ const CONFIG = {
     titleWeb: "Happi Anni 7M",
     introTitle: 'Nguyên Khoa',
     introDesc: `I don't know
-how you grow flowers insinde of me
-I wanna love you over and over again
-The sky is the limit with you`,
+how you grow flowers insinde of me 
+ I wanna love you over and over again
     btnIntro: 'ẤN VÀO ĐÂY ĐỂ TRẢ LỜI CÂU HỎI',
     title: 'Anh có yêu em hôngggg',
     desc: 'Chỉ được chọn một lần thui nên bấm chọn cho kỹ nha anhhhh',
@@ -16,5 +15,5 @@ The sky is the limit with you`,
     mess: 'Happi Anni 7M with uuuuu',
     messDesc: 'Một trò nho nhỏ, hi vọng anh sẽ thích hehe',
     btnAccept: 'YÊU ANH NHIỀU ♥️♥️♥️',
-    messLink: 'https://www.facebook.com/messages/t/100014188333536' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/messages/t/100002691802991' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
