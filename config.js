fe015b8fe256fe7d3c8726de229f4aa63867abd4
dntt315 <113818501+dntt315@users.noreply.github.com>
2022-09-18 14:21:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Happi Anni 7M",
     introTitle: 'Nguyên Khoa',
-    introDesc: `I wanna take you out
-Show you off to my friends
+    introDesc: `I don't know
+how you grow flowers insinde of me
 I wanna love you over and over again
-It's true, the sky is the limit with you`,
-    btnIntro: 'ẤN VÀO ĐÂY ĐỂ LÀM BÀI TEST',
+The sky is the limit with you`,
+    btnIntro: 'ẤN VÀO ĐÂY ĐỂ TRẢ LỜI CÂU HỎI',
     title: 'Anh có yêu em hôngggg',
-    desc: 'Thử chọn hông đi anh, thử thui nha',
-    btnYes: 'Cóoooo, anh iu bé nhiều',
-    btnNo: 'Hông bé ơi :<',
-    question: 'Khoảnh khắc nào khiến anh cảm thấy anh được yêu ?',
+    desc: 'Chỉ được chọn một lần thui nên bấm chọn cho kỹ nha anhhhh',
+    btnYes: 'Hông bé ơi :<',
+    btnNo: 'Cóoooo anh iu bé nhiều',
+    question: 'Khoảnh khắc nào khiến anh cảm thấy được yêu ?',
     btnReply: 'Gửi đến em iu',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Happi Anni 7M with uuuuu',
     messDesc: 'Một trò nho nhỏ, hi vọng anh sẽ thích hehe',
     btnAccept: 'YÊU ANH NHIỀU ♥️',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/messages/t/100014188333536' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
